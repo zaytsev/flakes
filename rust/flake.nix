68afcd2e26db98b69c8ba1aef9f980d6ea21d2a8
Sergey Zaytsev <zaytsev@users.noreply.github.com>
2021-10-08 14:15:13 +0700

    Use stable rust by default

--- rust/flake.nix
+++ rust/flake.nix
@@ -13,7 +13,7 @@
         let
           overlays = [ (import rust-overlay) ];
           pkgs = import nixpkgs { inherit system overlays; };
-          rust-bin = pkgs.rust-bin.nightly.latest.default.override {
+          rust-bin = pkgs.rust-bin.stable.latest.default.override {
             extensions = [ "rust-src" ];
             targets = [ "x86_64-unknown-linux-gnu" ];
             #targets = [ "x86_64-unknown-linux-gnu" "wasm32-unknown-unknown" "wasm32-wasi" ];
