ce4c6b2e42abc6c5812e933d4e638d6c120e0af4
Sergey Zaytsev <sergey@zaytseff.net>
2022-01-21 10:46:00 +0700

    Fix java flake missing pkgs declaration

--- java/flake.nix
+++ java/flake.nix
@@ -9,6 +9,9 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (
       system:
+      let
+      	pkgs = nixpkgs.legacyPackages.${system};
+      in
       {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
