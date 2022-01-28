572e45eabef197f417aebe19f2cceb0e81ff21a8
Sergey Zaytsev <sergey@zaytseff.net>
2022-01-28 18:26:14 +0700

    Update rust flake to use fenix

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,43 +1,42 @@
 {
-  description = "A very basic flake";
+  description = "Basic Rust dev environment";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, fenix }:
     flake-utils.lib.eachDefaultSystem (
       system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs { inherit system overlays; };
-          rust-bin = pkgs.rust-bin.stable.latest.default.override {
-            extensions = [ "rust-src" ];
-            targets = [ "x86_64-unknown-linux-gnu" ];
-            #targets = [ "x86_64-unknown-linux-gnu" "wasm32-unknown-unknown" "wasm32-wasi" ];
-          };
-        in
-          {
-            devShell = pkgs.mkShell {
-              buildInputs = with pkgs; [
-                rust-bin
+      let
+        overlays = [ fenix.overlay ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (
+              with fenix.packages.${system};
+              combine [
+                stable.rustc
+                stable.cargo
+                stable.rust-src
+                stable.rustfmt
                 rust-analyzer
-                cargo-generate
-                cargo-edit
-                cargo-update
-                cargo-geiger
-                cargo-outdated
-
-                # If the project requires openssl, uncomment these
-                #pkg-config
-                #openssl
-                #protobuf
-              ];
-              # If the project requires openssl, uncomment this
-              # PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-            };
-          }
+              ]
+            )
+            cargo-generate
+            cargo-edit
+            cargo-update
+            cargo-geiger
+            cargo-outdated
+          ];
+        };
+      }
     );
 }
