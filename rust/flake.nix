e33ebab1f2086c27f8b4ff8a7f1fa18c8283fde5
Sergey Zaytsev <sergey@zaytseff.net>
2025-04-12 00:11:41 +0700

    fix: Fix rust template

--- rust/flake.nix
+++ rust/flake.nix
@@ -4,89 +4,83 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = github:edolstra/flake-compat;
-      flake = false;
-    };
     fenix = {
-      url = "github:nix-community/fenix";
+      url = "github:nix-community/fenix/monthly";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, flake-compat, fenix }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    fenix,
+  }:
     flake-utils.lib.eachDefaultSystem
-      (
-        system:
-        let
-          overlays = [ fenix.overlays.default ];
-          pkgs = import nixpkgs { inherit system overlays; };
+    (
+      system: let
+        overlays = [fenix.overlays.default];
+        pkgs = import nixpkgs {inherit system overlays;};
 
-          projectConfig = (builtins.fromTOML (builtins.readFile ./Cargo.toml));
+        projectConfig = builtins.fromTOML (builtins.readFile ./Cargo.toml);
 
-          package =
-            let
-              toolchain = fenix.packages.${system}.stable.toolchain;
-            in
-            (pkgs.makeRustPlatform {
-              cargo = toolchain;
-              rustc = toolchain;
-            }).buildRustPackage {
-              pname = projectConfig.package.name;
-              version = projectConfig.package.version;
-              src = ./.;
-
-              doCheck = false;
-
-              nativeBuildInputs = with pkgs; [
-                pkg-config
-                openssl
-              ];
-
-              buildInputs = with pkgs; [
-                pkg-config
-                openssl
-              ];
-
-              cargoLock = {
-                lockFile = ./Cargo.lock;
-              };
-            };
+        package = let
+          toolchain = fenix.packages.${system}.stable.toolchain;
         in
-        {
-          packages = flake-utils.lib.flattenTree {
-            main = package;
+          (pkgs.makeRustPlatform {
+            cargo = toolchain;
+            rustc = toolchain;
+          })
+          .buildRustPackage {
+            pname = projectConfig.package.name;
+            version = projectConfig.package.version;
+            src = ./.;
+
+            doCheck = false;
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              openssl
+            ];
+
+            buildInputs = with pkgs; [
+              pkg-config
+              openssl
+            ];
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
           };
-          defaultPackage = packages.main;
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          main = package;
+        };
+        defaultPackage = packages.main;
 
-          devShell =
-            pkgs.mkShell
-              {
-                buildInputs = with pkgs; [
-                  (
-                    with fenix.packages.${system};
-                    combine [
-                      stable.rustc
-                      stable.cargo
-                      stable.rust-src
-                      stable.rustfmt
-                      rust-analyzer
-                    ]
-                  )
-                  cargo-edit
-                  cargo-update
-                  cargo-geiger
-                  cargo-outdated
-                  cargo-audit
+        devShell =
+          pkgs.mkShell
+          {
+            buildInputs = with pkgs; [
+              (
+                with fenix.packages.${system};
+                  combine [
+                    stable.rustc
+                    stable.cargo
+                    stable.rust-src
+                    stable.rustfmt
+                    # rust-analyzer
+                  ]
+              )
+              cargo-edit
+              cargo-update
+              cargo-geiger
+              cargo-outdated
+              cargo-audit
 
-                  cocogitto
-                  
-                  sqlx-cli
-
-                  openssl
-                  pkg-config
-                ];
-              };
-        }
-      );
+              cocogitto
+            ];
+          };
+      }
+    );
 }
